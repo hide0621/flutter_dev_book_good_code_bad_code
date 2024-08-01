ba965cbf01b2461ec0fa5c5dfa462597745a5354
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-08-02 08:59:53 +0900

    「buildメソッドの中でリストのフィルタリングはしない」例を書いてみた

--- chapter_10_2_1/lib/main.dart
+++ chapter_10_2_1/lib/main.dart
@@ -18,13 +18,23 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  /// ベストプラクティス
+  // const HomeScreen({super.key, required this.filteredItems});
+  // final List<Item> filteredItems;
+
   @override
   Widget build(BuildContext context) {
+    /// アンチパターン
     /// 画面遷移のためにNavigatorStateクラスを取得する以下の処理は
     /// ウィジェットの階層によっては計算量の大きな処理になる
     /// 不要な際は呼ばれないようにするためにも、
     /// 「ボタンがタップされた時」などのイベントに紐付けて呼ばれるべき
     // final navigator = Navigator.of(context);
+
+    /// アンチパターン
+    /// 以下のような実装ではbuildメソッドが呼ばれるたびにリストのフィルタリングが行われてしまうため、
+    /// あらかじめフィルタリングしたリストをウィジェットに渡してbuildメソッドの実行コストを下げるべき
+    // final filteredItems = items.where((item) => /* itemのフィルタ条件 */).toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -33,6 +43,7 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
+            /// ベストプラクティス
             final navigator = Navigator.of(context);
             navigator.push(
               MaterialPageRoute(
