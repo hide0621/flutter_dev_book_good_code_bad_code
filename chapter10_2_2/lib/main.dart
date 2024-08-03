66a7116068cd28533a34e6751155198798c441b2
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-08-03 10:57:33 +0900

    「biildメソッドで大きなウィジェットツリーを構築しない」のベストプラクティスを書いてみた

--- chapter10_2_2/lib/main.dart
+++ chapter10_2_2/lib/main.dart
@@ -32,24 +32,46 @@ class HomeScreen extends StatelessWidget {
       /// ウィジェットを右下に配置したい場合、RowウィジェットとColumnウィジェットを組み合わせて実現できるが、
       /// それだとウィジェットの階層が深くなってしまい再構築するウィジェットも増え、
       /// Elementの再利用判定などのコストが高くなってしまう
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.end, // 右寄せ
-        children: [
-          Column(
-              mainAxisAlignment: MainAxisAlignment.end, // 下寄せ
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const DetailScreen(),
-                      ),
-                    );
-                  },
-                  child: const Text('Go to Detail'),
-                )
-              ])
-        ],
+      /// 加えて、可読性も落ちる
+      // body: Row(
+      //   mainAxisAlignment: MainAxisAlignment.end, // 右寄せ
+      //   children: [
+      //     Column(
+      //         mainAxisAlignment: MainAxisAlignment.end, // 下寄せ
+      //         children: [
+      //           ElevatedButton(
+      //             onPressed: () {
+      //               Navigator.of(context).push(
+      //                 MaterialPageRoute(
+      //                   builder: (context) => const DetailScreen(),
+      //                 ),
+      //               );
+      //             },
+      //             child: const Text('Go to Detail'),
+      //           )
+      //         ])
+      //   ],
+      // ),
+
+      /// ベストプラクティス
+      /// 上記のようなレイアウトならAlignウィジェット1つで実現できる
+      /// これにより再構築するウィジェットが少なくなり、Elementの再利用判定などのコストが低くなる
+      /// 加えて、可読性も向上する
+
+      /// Alignウィジェット
+      /// alignmentプロパティで子ウィジェットの配置を指定できる
+      body: Align(
+        alignment: Alignment.bottomRight, // 右下寄せ
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const DetailScreen(),
+              ),
+            );
+          },
+          child: const Text('Go to Detail'),
+        ),
       ),
     );
   }
