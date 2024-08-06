80a020b983880f3bb10283f23a1fa1dd0f1b619c
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-08-06 09:26:32 +0900

    「状態を末端のウィジェットに移す」のベストプラクティスなコードを書いてみた

--- chapter10_2_4/lib/main.dart
+++ chapter10_2_4/lib/main.dart
@@ -6,14 +6,71 @@ void main() {
   ));
 }
 
-class HomeScreen extends StatefulWidget {
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({super.key});
+//
+//   @override
+//   State createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   int _counter = 0;
+//
+//   void increment() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+//
+//   /// アンチパターン
+//   /// ボタンタップの度にScaffoldウィジェットやAppBarウィジェットも再構築されてしまう
+//   /// 状態更新があった際[_counter]に、その状態とは関係のないウィジェットまで再構築されることは避けるべき
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Home Screen'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           child: Text('count = $_counter'),
+//           onPressed: () => increment(),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  /// ベストプラクティス
+  /// CountButtonウィジェットを作成し、そのウィジェット内で状態[_counter]を管理する
+  /// こうすることで、CountButtonウィジェットの状態更新時には、CountButtonウィジェットのみが再構築される
+  ///
+  /// また関心事を分けることにも繋がり、HomeScreen画面のコードからカウントアップのロジックを分離することができた
+  /// これにより保守性が高まり、CountButtonウィジェットは再利用性も確保された
   @override
-  State createState() => _HomeScreenState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+        child: CountButton(),
+      ),
+    );
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class CountButton extends StatefulWidget {
+  const CountButton({super.key});
+
+  @override
+  State createState() => _CountButtonState();
+}
+
+class _CountButtonState extends State<CountButton> {
   int _counter = 0;
 
   void increment() {
@@ -22,21 +79,11 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  /// アンチパターン
-  /// ボタンタップの度にScaffoldウィジェットやAppBarウィジェットも再構築されてしまう
-  /// 状態更新があった際に、その状態とは関係のないウィジェットまで再構築されることは避けるべき
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('count = $_counter'),
-          onPressed: () => increment(),
-        ),
-      ),
+    return ElevatedButton(
+      child: Text('count = $_counter'),
+      onPressed: () => increment(),
     );
   }
 }
