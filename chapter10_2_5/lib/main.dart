c306f28a5d36dd741dcf9a63d12294fa2e885a73
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-08-08 09:22:10 +0900

    riverpodを使った状態監視のアンチパターンを書いてみた

--- chapter10_2_5/lib/main.dart
+++ chapter10_2_5/lib/main.dart
@@ -21,28 +21,39 @@ void main() {
   );
 }
 
-// class HomeScreen extends ConsumerWidget {
-//   const HomeScreen({super.key});
-//
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Home Screen'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               ref.watch();
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+
+            /// アンチパターン
+            ///
+            /// 状態監視をしてその更新が確認される度にTextウィジェットも再構築される
+            /// つまり、このTextウィジェットの再描画のためにHomeScreenウィジェットのbuildメソッドも再実行されるので効率が悪い
+            Text(
+              '${ref.watch(counterProvider)}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ref.read(counterProvider.notifier).increment(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
